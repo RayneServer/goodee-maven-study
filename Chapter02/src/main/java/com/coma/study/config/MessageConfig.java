25dde8a39ea5273bb739c9adcf39e6f537c0aca1
Rayne <pluvstream@gmail.com>
2025-08-14 17:49:16 +0900

    Update: 언어 변경 실습

---
+++ Chapter02/src/main/java/com/coma/study/config/MessageConfig.java
@@ -0,0 +1,42 @@
+package com.coma.study.config;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class MessageConfig implements WebMvcConfigurer {
+
+	@Bean
+	LocaleResolver localeResolver() {
+		// Session에 보관하는 경우
+		SessionLocaleResolver sessionResolver = new SessionLocaleResolver();
+		sessionResolver.setDefaultLocale(Locale.KOREAN);
+	
+		// Cookie에 보관하는 경우
+		CookieLocaleResolver cookieResolver= new CookieLocaleResolver("lang");
+		cookieResolver.setDefaultLocale(Locale.KOREAN);
+		
+		return cookieResolver;
+	}
+	
+	LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+		interceptor.setParamName("lang");
+		
+		return interceptor;
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(this.localeChangeInterceptor())
+						.addPathPatterns("/**");
+	}
+}
